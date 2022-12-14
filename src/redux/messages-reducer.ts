33f8abc0b472aeeff50457a57e78732cb650d2b6
Valera <valerakaktotak@gmail.com>
2022-12-14 21:23:05 +0200

    message-reducer refactoring initial state TS

--- src/redux/messages-reducer.ts
+++ src/redux/messages-reducer.ts
@@ -35,29 +35,31 @@ type messageDataType = {
     id: number
     message: string
 }
-type messagesReducerInitType = {
-    dialogData: Array<dialogDataType>
-    messageData: Array<messageDataType>
-    newMessageDataArea: string
-}
-const messagesReducerInit: messagesReducerInitType = {
+// type messagesReducerInitType = {
+//     dialogData: Array<dialogDataType>
+//     messageData: Array<messageDataType>
+//     newMessageDataArea: string
+// }
+const messagesReducerInit = {
     dialogData: [
         {id: 1, name: 'Valera'},
         {id: 2, name: 'Dima'},
         {id: 3, name: 'Sveta'},
         {id: 4, name: 'Marina'},
         {id: 5, name: 'Lena'},
-    ],
+    ] as Array<dialogDataType>,
     messageData: [
         {id: 1, message: 'Hi:)'},
         {id: 2, message: 'hi hi hi:)'},
         {id: 3, message: 'Hello!)))'},
         {id: 4, message: 'How are you?)'},
         {id: 5, message: 'Are you ok?'},
-    ],
-    newMessageDataArea: ''
+    ] as Array<messageDataType>,
+    newMessageDataArea: '' as string
 }
-const messagesReducer = (state = messagesReducerInit, action: changeMessageTextActionType | addMessageActionType) => {
+type messagesReducerInitType = typeof messagesReducerInit
+
+const messagesReducer = (state = messagesReducerInit, action: changeMessageTextActionType | addMessageActionType):messagesReducerInitType => {
     if (action.type === changeMessageTextActionCreatorConst) {
         return {
             ...state,
